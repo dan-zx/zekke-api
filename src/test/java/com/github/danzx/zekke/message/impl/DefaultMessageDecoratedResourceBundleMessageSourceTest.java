4769e763054b89eab41685d128dbfa0dcc3fed4b
Dan <danyboy_shaggy@hotmail.com>
2017-05-29 21:21:06 -0500

    Enhance messages test

--- src/test/java/com/github/danzx/zekke/message/impl/DefaultMessageDecoratedResourceBundleMessageSourceTest.java
+++ src/test/java/com/github/danzx/zekke/message/impl/DefaultMessageDecoratedResourceBundleMessageSourceTest.java
@@ -15,19 +15,23 @@
  */
 package com.github.danzx.zekke.message.impl;
 
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
 import com.github.danzx.zekke.message.MessageSource;
 import com.github.danzx.zekke.test.paramprovider.BlankStringProvider;
+
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -46,10 +50,11 @@ public class DefaultMessageDecoratedResourceBundleMessageSourceTest {
     }
 
     @Test
-    @Parameters(method = "messagesWithoutArgs")
-    public void shouldGetExpectedMessageWithNoExceptions(String messageKey, String expectedMessage) {
-        String actualMessage = TEST_MESSAGES.getMessage(messageKey);
-        assertThat(actualMessage).isNotNull().isNotEmpty().isEqualTo(expectedMessage);
+    @Parameters(method = "constructorArgs")
+    public void shouldBuildResourceBundleMessageSource(String basename, Set<Locale> locales) {
+        ResourceBundleMessageSource messageSource = locales == null ? new ResourceBundleMessageSource(basename) : new ResourceBundleMessageSource(basename, locales);
+        assertThat(messageSource.getBasename()).isNotNull().isNotBlank().isEqualTo(basename);
+        assertThat(messageSource.getSupportedLocales()).isNotNull().isNotEmpty().containsOnly(Locale.ROOT);
     }
 
     @Test
@@ -69,43 +74,58 @@ public class DefaultMessageDecoratedResourceBundleMessageSourceTest {
     }
 
     @Test
-    public void shouldGetMessageUsingArguments() {
-        List<String> args = Arrays.asList("a1arg", "2");
-        String actualMessage = TEST_MESSAGES.getMessage("test.message_with_args", args.get(0), args.get(1));
-        assertThat(actualMessage).isNotNull().isNotEmpty().contains(args).isEqualTo("Test message with a1arg and 2");
+    @Parameters(method = "messagesWithoutArgs")
+    public void shouldGetExpectedMessage(String messageKey, Locale locale, String expectedMessage) {
+        String actualMessage = locale == null ? TEST_MESSAGES.getMessage(messageKey) : TEST_MESSAGES.getMessage(messageKey, locale);
+        assertThat(actualMessage).isNotNull().isNotEmpty().isEqualTo(expectedMessage);
     }
 
     @Test
-    public void shouldGetLocalizedMessage() {
-        String actualMessage = TEST_MESSAGES.getMessage("test.message", SPANISH);
-        assertThat(actualMessage).isNotNull().isNotEmpty().isEqualTo("Mensage de prueba");
+    @Parameters(method = "messagesWithArgs")
+    public void shouldGetMessageUsingArguments(String messageKey, Locale locale, Object[] args, String expectedMessage) {
+        String actualMessage = locale == null ? TEST_MESSAGES.getMessage(messageKey, args) : TEST_MESSAGES.getMessage(messageKey, locale, args);
+        assertThat(actualMessage).isNotNull().isNotEmpty().isEqualTo(expectedMessage);
     }
 
-    @Test
-    public void shouldGetLocalizedMessageWithArgs() {
-        List<String> args = Arrays.asList("a1arg", "2");
-        String actualMessage = TEST_MESSAGES.getMessage("test.message_with_args", SPANISH, args.get(0), args.get(1));
-        assertThat(actualMessage).isNotNull().isNotEmpty().contains(args).isEqualTo("Mensage de prueba con a1arg y 2");
-    }
-
-    @Test
-    public void shouldGetRootLocalizedMessageWhenLocaleIsNotSupported() {
-        String actualMessage = TEST_MESSAGES.getMessage("test.message", Locale.FRENCH);
-        assertThat(actualMessage).isNotNull().isNotEmpty().isEqualTo("Test message");
-    }
-
-    @Test
-    public void shouldGetRootLocalizedMessageWithArgsWhenLocaleIsNotSupported() {
-        List<String> args = Arrays.asList("a1arg", "2");
-        String actualMessage = TEST_MESSAGES.getMessage("test.message_with_args", Locale.FRENCH, args.get(0), args.get(1));
-        assertThat(actualMessage).isNotNull().isNotEmpty().contains(args).isEqualTo("Test message with a1arg and 2");
+    protected Object[] constructorArgs() {
+        return new Object[][] {
+            {"messages.TestMessages", null},
+            {"messages.TestMessages", emptySet()},
+            {"messages.TestMessages", singleton(Locale.ROOT)},
+        };
     }
 
     protected Object[] messagesWithoutArgs() {
         String nonExitingMessage = "non_existing.message";
-        return new Object[][]{
-                {"test.message", "Test message"},
-                {nonExitingMessage, String.format(MISSING_KEY_FORMAT, nonExitingMessage)}
+        String expectedMessageWithNonExitingKey = String.format(MISSING_KEY_FORMAT, nonExitingMessage);
+        return new Object[][] {
+            {"test.message", null, "Test message"},
+            {nonExitingMessage, null, expectedMessageWithNonExitingKey},
+            {"test.message", Locale.FRENCH, "Test message"},
+            {nonExitingMessage, Locale.FRENCH, expectedMessageWithNonExitingKey},
+            {"test.message", SPANISH, "Mensage de prueba"},
+            {nonExitingMessage, SPANISH, expectedMessageWithNonExitingKey}
+        };
+    }
+
+    protected Object[] messagesWithArgs() {
+        String nonExitingMessage = "non_existing.message";
+        String expectedMessageWithNonExitingKey = String.format(MISSING_KEY_FORMAT, nonExitingMessage);
+        Object[] args = {"a1arg", "2"};
+        Object[] arg = {"lol"};
+        return new Object[][] {
+            {"test.message_with_arg", null, arg, "Test message with lol"},
+            {nonExitingMessage, null, null, expectedMessageWithNonExitingKey},
+            {"test.message_with_arg", Locale.FRENCH, arg, "Test message with lol"},
+            {nonExitingMessage, Locale.FRENCH, null, expectedMessageWithNonExitingKey},
+            {"test.message_with_arg", SPANISH, arg, "Mensage de prueba con lol"},
+            {nonExitingMessage, SPANISH, null, expectedMessageWithNonExitingKey},
+            {"test.message_with_args", null, args, "Test message with a1arg and 2"},
+            {nonExitingMessage, null, null, expectedMessageWithNonExitingKey},
+            {"test.message_with_args", Locale.FRENCH, args, "Test message with a1arg and 2"},
+            {nonExitingMessage, Locale.FRENCH, null, expectedMessageWithNonExitingKey},
+            {"test.message_with_args", SPANISH, args, "Mensage de prueba con a1arg y 2"},
+            {nonExitingMessage, SPANISH, null, expectedMessageWithNonExitingKey}
         };
     }
 }
