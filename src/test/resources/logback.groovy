f1d2850a6d5b5fd1067def2eaa3623f0f732c909
Dan <danyboy_shaggy@hotmail.com>
2018-06-16 23:32:47 -0500

    Route JUL logs to SLF4J

--- src/test/resources/logback.groovy
+++ src/test/resources/logback.groovy
@@ -1,3 +1,17 @@
+import ch.qos.logback.classic.jul.LevelChangePropagator
+
+import org.slf4j.bridge.SLF4JBridgeHandler
+
+def lcp = new LevelChangePropagator()
+lcp.context = context
+lcp.resetJUL = true
+context.addListener(lcp)
+
+java.util.logging.LogManager.getLogManager().reset()
+SLF4JBridgeHandler.removeHandlersForRootLogger()
+SLF4JBridgeHandler.install()
+java.util.logging.Logger.getLogger('global').setLevel(java.util.logging.Level.FINEST)
+
 appender('console', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = '%d{yyyy/MM/dd HH:mm:ss.SSS} %-5level in %logger.%method\\(\\) - %message%n'
@@ -13,13 +27,15 @@ logger('org.apache.catalina', WARN)
 logger('org.apache.commons.beanutils', WARN)
 logger('org.apache.coyote', WARN)
 logger('org.apache.tomcat', WARN)
-logger('org.glassfish.jersey', WARN)
+logger('org.glassfish', WARN)
+logger('org.glassfish.jersey.server.spring.SpringComponentProvider', OFF)
 logger('org.hibernate.validator', WARN)
 logger('org.jboss.logging', WARN)
 logger('org.jvnet.hk2', WARN)
 logger('org.mongodb.driver', WARN)
 logger('org.mongodb.morphia', WARN)
 logger('org.springframework', WARN)
+logger('sun.net', WARN)
 
 logger('com.github.danzx.zekke.test.mongo.EmbeddedMongo', WARN)
 
